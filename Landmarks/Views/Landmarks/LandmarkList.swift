dda6eb50ac662d50d506ca59fe7de056f646d68e
Tao.T <1120135081@qq.com>
2021-01-19 16:00:39 +0800

    处理用户事件

---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,48 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Tao.T on 2021/1/13.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(
+                        destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+        
+    }
+    
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
